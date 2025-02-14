ef647a1aa065e301cdb22029a6db11b868bc01d3
MariiaOtset <152022954+MariiaOtset@users.noreply.github.com>
2025-02-14 15:45:54 +0000

    basic components added to registration screen

--- app/Screens/RegistrationScreen.tsx
+++ app/Screens/RegistrationScreen.tsx
@@ -1,3 +1,4 @@
+import CustomButton from "@/components/CustomButton";
 import React from "react";
 import {
   Button,
@@ -5,23 +6,47 @@ import {
   StyleSheet,
   Text,
   View,
-  TouchableOpacity,
+  TextInput,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const RegistrationScreen = () => {
+  const register = () => {
+    console.log("Registering...");
+  };
+
   return (
-    <SafeAreaView style={styles.container}>
-      <ImageBackground
-        style={styles.background}
-        resizeMode="cover"
-        source={require("@/assets/images/mountains.jpg")}
-      >
-        <View>
-          <Text style={styles.text}>Registration</Text>
-          <Button title="Register" color={styles.button.backgroundColor} />
-        </View>
-      </ImageBackground>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <ImageBackground
+          style={styles.background}
+          resizeMode="cover"
+          source={require("@/assets/images/mountains.jpg")}
+        >
+          <View style={styles.formContainer}>
+            <Text style={styles.text}>Registration</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Enter your login"
+              //   autoComplete="email"
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Enter your email address"
+              autoComplete="email"
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Enter your password"
+              autoComplete="password"
+            />
+            <CustomButton
+              title="Register"
+              onPress={(event) => console.log(event)}
+            />
+          </View>
+        </ImageBackground>
+      </View>
     </SafeAreaView>
   );
 };
@@ -32,16 +57,17 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
-  text: { fontSize: 28, lineHeight: 32, marginTop: -6 },
   background: {
     flex: 1,
     width: "100%",
     height: "100%",
   },
-  button: {
-    borderRadius: "100px",
-    backgroundColor: "#FF6C00",
+  formContainer: {
+    marginTop: 219,
+    backgroundColor: "#FFF",
   },
+  text: { fontSize: 28, lineHeight: 32, marginTop: -6 },
+  input: {},
 });
 
 export default RegistrationScreen;
---
+++ components/CustomButton.tsx
@@ -0,0 +1,32 @@
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import { GestureResponderEvent } from "react-native";
+
+interface CustomButtonProps {
+  onPress: (event: GestureResponderEvent) => void;
+  title: string;
+}
+
+const CustomButton: React.FC<CustomButtonProps> = ({ onPress, title }) => (
+  <TouchableOpacity style={styles.button} onPress={onPress}>
+    <Text style={styles.buttonText}>{title}</Text>
+  </TouchableOpacity>
+);
+
+const styles = StyleSheet.create({
+  button: {
+    margin: 16,
+    marginTop: 43,
+    paddingVertical: 16,
+    paddingHorizontal: 32,
+    borderRadius: 100,
+    backgroundColor: "#FF6C00",
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
+
+export default CustomButton;
