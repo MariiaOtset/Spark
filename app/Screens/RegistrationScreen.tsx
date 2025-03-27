6bbb2a0d3a4ff3099bdc4c218e07113a1d1ffba4
MariiaOtset <152022954+MariiaOtset@users.noreply.github.com>
2025-03-27 20:48:31 +0000

    avatar fixed, new styles for container added

--- app/Screens/RegistrationScreen.tsx
+++ app/Screens/RegistrationScreen.tsx
@@ -13,6 +13,7 @@ import {
   Image,
   Alert,
   Button,
+  ScrollView,
 } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 
@@ -33,7 +34,7 @@ const RegistrationScreen = () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
-      aspect: [1, 1], // Квадратное изображение
+      aspect: [1, 1],
       quality: 1,
     });
 
@@ -59,21 +60,18 @@ const RegistrationScreen = () => {
             >
               <View style={styles.formContainer}>
                 <View style={{ alignItems: "center", marginTop: 20 }}>
-                  <Image
-                    source={
-                      avatar
-                        ? { uri: avatar }
-                        : require("@/assets/images/add_photo.png")
-                    }
-                    style={{
-                      width: 120,
-                      height: 120,
-                      borderRadius: 14,
-                      borderWidth: 2,
-                      borderColor: "rgba(246, 246, 246, 1)",
-                    }}
-                  />
-                  <Button title="фото" onPress={pickImage} />
+                  <View style={styles.avatarContainer}>
+                    <Pressable onPress={pickImage}>
+                      <Image
+                        source={
+                          avatar
+                            ? { uri: avatar }
+                            : require("@/assets/images/add_photo.png")
+                        }
+                        style={styles.avatar}
+                      />
+                    </Pressable>
+                  </View>
                 </View>
                 <Text style={styles.title}>Registration</Text>
 
@@ -123,6 +121,83 @@ const RegistrationScreen = () => {
         </KeyboardAvoidingView>
       </Pressable>
     </SafeAreaView>
+    // <SafeAreaView style={{ flex: 1 }}>
+    //   <ImageBackground
+    //     style={styles.background}
+    //     resizeMode="cover"
+    //     source={require("@/assets/images/mountains.jpg")}
+    //   >
+    //     <KeyboardAvoidingView
+    //       behavior={Platform.OS == "ios" ? "padding" : "height"}
+    //       style={{ flex: 1 }}
+    //     >
+    //       <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+    //         <Pressable onPress={Keyboard.dismiss} style={{ flex: 1 }}>
+    //           <View style={styles.container}>
+    //             <View style={styles.formContainer}>
+    //               <View style={{ alignItems: "center", marginTop: 20 }}>
+    //                 <View style={styles.avatarContainer}>
+    //                   <Pressable onPress={pickImage}>
+    //                     <Image
+    //                       source={
+    //                         avatar
+    //                           ? { uri: avatar }
+    //                           : require("@/assets/images/add_photo.png")
+    //                       }
+    //                       style={styles.avatar}
+    //                     />
+    //                   </Pressable>
+    //                 </View>
+    //               </View>
+    //               <Text style={styles.title}>Registration</Text>
+
+    //               <CustomInput
+    //                 placeholder="Login"
+    //                 value={login}
+    //                 onChangeText={setLogin}
+    //               />
+    //               <CustomInput
+    //                 placeholder="Email address"
+    //                 value={email}
+    //                 onChangeText={setEmail}
+    //                 autoComplete="email"
+    //               />
+    //               <View style={styles.inputContainer}>
+    //                 <CustomInput
+    //                   placeholder="Password"
+    //                   value={password}
+    //                   onChangeText={setPassword}
+    //                   secureTextEntry={!isPasswordVisible}
+    //                   autoComplete="password"
+    //                   style={{ marginBottom: 0 }}
+    //                 />
+    //                 <Pressable
+    //                   onPress={togglePasswordVisibility}
+    //                   style={styles.toggleButton}
+    //                 >
+    //                   <Text style={[styles.textBase, styles.toggleText]}>
+    //                     {isPasswordVisible ? "Hide" : "Show"}
+    //                   </Text>
+    //                 </Pressable>
+    //               </View>
+
+    //               <CustomButton
+    //                 title="Register"
+    //                 onPress={(event) => console.log(event)}
+    //               />
+    //               <Text style={[styles.textBase, styles.loginLink]}>
+    //                 Already have an account?
+    //                 <Text style={[styles.textBase, styles.loginLink]}>
+    //                   Log in
+    //                 </Text>
+    //               </Text>
+    //             </View>
+    //           </View>
+    //         </Pressable>
+    //       </ScrollView>
+    //     </KeyboardAvoidingView>
+    //   </ImageBackground>
+    // </SafeAreaView>
   );
 };
 
@@ -140,13 +215,14 @@ const styles = StyleSheet.create({
   formContainer: {
     marginTop: "auto",
     padding: 16,
+    paddingBottom: 45,
     height: 549,
     borderTopLeftRadius: 25,
     borderTopRightRadius: 25,
     backgroundColor: "#FFF",
   },
   title: {
-    marginTop: 32,
+    marginTop: 63,
     marginBottom: 33,
     color: "#212121",
     textAlign: "center",
@@ -162,6 +238,18 @@ const styles = StyleSheet.create({
     fontSize: 16,
     lineHeight: 16,
     fontFamily: "Roboto-Regular",
+  },
+  avatarContainer: {
+    position: "absolute",
+    top: "50%", // Находится на 50% от начала formContainer
+    left: "50%",
+    transform: [{ translateX: -66 }, { translateY: -95 }], // Сдвиг для центрирования (половина ширины и высоты аватара)
+    // zIndex: 1,
+  },
+  avatar: {
+    width: 132,
+    height: 120,
+    borderRadius: 14,
   },
   toggleButton: {
     position: "absolute",
